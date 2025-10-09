4488487cb60dafc1a3b9151658a181377cb3786a
DefffinitiveOx72 <166840943+DefffinitiveOx72@users.noreply.github.com>
2025-10-09 12:27:48 +0300

    modify function.js

--- js/functions.js
+++ js/functions.js
@@ -1,26 +1,59 @@
-const checkLengthString = (string, maxLength) => string.length <= maxLength;
+// Функция для проверки длины строки.
+// Она принимает строку, которую нужно проверить, и максимальную длину и возвращает true, если строка меньше или равна указанной длине, и false, если строка длиннее.
+// Эта функция нам пригодится для валидации формы.
+const getStringLength = (string, maxLength) => string.length <= maxLength;
 
-const checkPalindrome = (string) => {
-  const normalString = string.replaceAll(' ','').toLowerCase();
-  let reverseString = '';
-  for (let i = normalString.length - 1; i >= 0 ; i--) {
-    reverseString += normalString[i];
-  }
-  return normalString === reverseString;
-};
+getStringLength('привет', 10);
 
+// Функция для проверки, является ли строка палиндромом.
+// Палиндром — это слово или фраза, которые одинаково читаются и слева направо и справа налево.
 
-const extractNumbers = (string) => {
-  const normalString = string.toString().replaceAll(' ','');
-  let stringNumbers = '';
-  for (let i = 0; i < normalString.length ; i++) {
-    if (normalString[i] >= 0 && normalString[i] <= 9) {
-      stringNumbers += normalString[i];
+const isStringPalindrome = (string) => {
+  const modifiedString = string.replaceAll(' ', '').toLowerCase();
+  let result = 0;
+
+  for (let i = 0; i <= modifiedString.length - 1; i++) {
+    if (modifiedString[i] === modifiedString[modifiedString.length - i - 1]) {
+      result++;
     }
   }
-  return parseInt(stringNumbers, 10);
+
+  return result === modifiedString.length;
 };
 
-checkLengthString ('дом мод',5);
-checkPalindrome ('дом мод');
-extractNumbers ('2 счастливых гуся и 11 гусынь');
+isStringPalindrome('Лёша на полке клопа нашёл ');
+
+// Функция принимает строку, извлекает содержащиеся в ней цифры от 0 до 9 и возвращает их в виде целого положительного числа.
+// Если в строке нет ни одной цифры, функция должна вернуть NaN:
+
+// имяФункции('2023 год');            // 2023
+// имяФункции('ECMAScript 2022');     // 2022
+// имяФункции('1 кефир, 0.5 батона'); // 105
+// имяФункции('агент 007');           // 7
+// имяФункции('а я томат');           // NaN
+
+const getNumbers = function (string) {
+  const modifiedString = string.replaceAll(' ', '').toLowerCase();
+  let result = '';
+
+  for (let i = 0; i <= modifiedString.length; i++) {
+    for (let j = 0; j <= 9; j++) {
+      if (Number(modifiedString[i]) === j) {
+        result += j;
+      }
+    }
+  }
+  result = Number(result);
+  if (result === 0) {
+    result = NaN;
+  }
+  return result;
+};
+
+getNumbers('0029 of June 2024 year');
+
+// Если хотите усложнить задание, предусмотрите случай, когда вместо строки приходит число. Обратите внимание, что возвращать функция по-прежнему должна только целые положительные числа:
+
+// имяФункции(2023); // 2023
+// имяФункции(-1);   // 1
+// имяФункции(1.5);  // 15
